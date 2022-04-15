411da4dd9bf94a8ea66516b267a24972feea06b1
Bertrand <delphine.bertrand@editus.lu>
2022-04-15 11:41:47 +0200

    header

---
+++ front/src/layouts/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <header className="main-header">
+        <img src ={process.env.PUBLIC_URL + 'logo.png'} alt ="logo oh my food" className="logo"></img>
+    </header>
+  );
+}
+
+export default Header;
