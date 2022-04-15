45e9242480de7b95fa346c1cd0734485367528b8
Bertrand <delphine.bertrand@editus.lu>
2022-04-15 15:16:15 +0200

    typography

--- front/src/pages/Homepage.js
+++ front/src/pages/Homepage.js
@@ -2,9 +2,14 @@ import Searchbar from "../components/Searchbar";
 
 function Homepage() {
     return (
-      <>
+      <div id="homepage">
         <Searchbar></Searchbar>
-      </>
+        <section className="section-discover">
+            <h1 className="main-title">Réservez le menu qui vous convient</h1>
+            <p>Découvrez de nouveaux restaurants d'exception, sélectionnés par nos soins</p>
+            <a href="toto" className="button primary">Explorez nos restaurants</a>
+        </section>
+      </div>
     );
   }
   
