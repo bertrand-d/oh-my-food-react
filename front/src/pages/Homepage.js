00546ca750c365104f2fae758394e31d82432a46
Bertrand <delphine.bertrand@editus.lu>
2022-04-15 15:35:03 +0200

    section discover

--- front/src/pages/Homepage.js
+++ front/src/pages/Homepage.js
@@ -7,7 +7,7 @@ function Homepage() {
         <section className="section-discover">
             <h1 className="main-title">Réservez le menu qui vous convient</h1>
             <p>Découvrez de nouveaux restaurants d'exception, sélectionnés par nos soins</p>
-            <a href="toto" className="button primary">Explorez nos restaurants</a>
+            <a href="toto" className="button">Explorez nos restaurants</a>
         </section>
       </div>
     );
