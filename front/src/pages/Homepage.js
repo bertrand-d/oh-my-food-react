7a2660cf2b9f58a9a776f49b4df8593940abdf65
Bertrand <delphine.bertrand@editus.lu>
2022-04-19 15:45:05 +0200

    section fonctionment

--- front/src/pages/Homepage.js
+++ front/src/pages/Homepage.js
@@ -9,6 +9,27 @@ function Homepage() {
             <p>Découvrez de nouveaux restaurants d'exception, sélectionnés par nos soins</p>
             <a href="toto" className="button">Explorez nos restaurants</a>
         </section>
+        <section className="section-fonctionment max-container">
+            <h2 className="second-title">Fonctionnement</h2>
+            <div className="buttons-container">
+                <button className= "button-function">
+                    <span className="number">1</span>
+                    <span className="icon phone"></span>
+                    Choisissez un restaurant
+                </button>
+                <button className= "button-function">
+                    <span className="number">2</span>
+                    <span className="icon list"></span>
+                    Composez votre menu
+                </button>
+                <button className= "button-function">
+                    <span className="number">3</span>
+                    <span className="icon store"></span>
+                    Dégustez au restaurant
+                </button>
+            </div>
+
+        </section>
       </div>
     );
   }
