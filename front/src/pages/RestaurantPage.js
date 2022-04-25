ebf75022b5abc244ddbf17d3d0afcdbea98f2af0
Bertrand <delphine.bertrand@editus.lu>
2022-04-25 11:56:44 +0200

    category title

--- front/src/pages/RestaurantPage.js
+++ front/src/pages/RestaurantPage.js
@@ -5,19 +5,20 @@ function RestaurantPage() {
                 <img src={"/restaurants/la_note_enchantee.jpg" } alt="restaurant"></img>
             </div>
             <section className="menu">
-                <h2 className="main-subtitle-script">La palette du goût</h2>
-                <div className="menu-container">
-                    <div className="menu-column">
-                        <h3 className="starters">Entrées</h3>
-                    </div>
-                    <div className="menu-column">
-                        <h3 className="dishes">Plats</h3>
-                    </div>
-                    <div className="menu-column">
-                        <h3 className="desserts">Desserts</h3>
-                    </div>
-                </div>
-                
+                <div className="menu-content">
+                    <h2 className="second-title script">La palette du goût</h2>
+                    <div className="menu-container">
+                        <div className="menu-column">
+                            <h3 className="category-title">Entrées</h3>
+                        </div>
+                        <div className="menu-column">
+                            <h3 className="category-title">Plats</h3>
+                        </div>
+                        <div className="menu-column">
+                            <h3 className="category-title">Desserts</h3>
+                        </div>
+                    </div>    
+                </div>     
             </section>
         </div>
     );
